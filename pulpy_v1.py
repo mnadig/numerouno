f77b037d9a609de95132b64359120095282b5e2b
Madhavi Nadig <mnadig@spotify.com>
2016-12-18 20:53:17 -0500

    PuLP : Use dictionaries instead of individual variables

--- pulpy_v1.py
+++ pulpy_v1.py
@@ -1,6 +1,6 @@
 # PuLP documentation : https://pythonhosted.org/PuLP/CaseStudies/a_blending_problem.html
 
-# Version 2 : Manage variables using dictionaries
+# Version 1 : Manage variables individually
 
 from pulp import *
 
@@ -13,38 +13,41 @@ print "Investment amount = ", amount
 print "Max % of portfolio per scrip = ", max_percentage_of_portfolio
 print "-----"
 
-
 # i is the scrip
 price = {'SINO': 1.2, 'AAPL': 100.0, 'FSAM': 5.9}
-scrips = price.keys()
 change = {'SINO': 0.2, 'AAPL': 1.01, 'FSAM': 0.4}
 days = {'SINO': 5.0, 'AAPL': 30.0, 'FSAM': 15.0}
 
 
-# Create the 'prob' variable to contain the problem data
 prob = LpProblem("Portfolio Gain Prediction", LpMaximize)
 
+# a_i == # of shares of scrip i
+a_1 = LpVariable("nSINO", 0, None, LpInteger)
+a_2 = LpVariable("nAAPL", 0, None, LpInteger)
+a_3 = LpVariable("nFSAM", 0, None, LpInteger)
 
-# A dictionary called volume_vars is created to contain the referenced Variables
-volume_vars = LpVariable.dicts("n", scrips, lowBound=0, cat=LpInteger)
 
-
-# The objective function is added to 'prob' first
-# Objective : Maximize gain_percent
+# Objective problem : Maximize gain_percent
 # gain_per_day = ndays / amount * 100 * sum ( c_i / d_i)
-m_const = ndays / amount * 100.0
+m_const = ndays / amount * 100
 gain_per_day = {k : m_const * v / days[k] for k, v in change.items()}
-prob += lpSum([gain_per_day[i] * volume_vars[i] for i in scrips]), "Expected gain %"
+prob += gain_per_day['SINO'] * a_1 + gain_per_day['AAPL'] * a_2 + gain_per_day['FSAM'] * a_3, \
+        "Expected gain"
 
 
-# Constraint on investment in portfolio
+# Constraint on value of portfolio
 # sum ( p_i * a_i ) == amount
-prob += lpSum([price[i] * volume_vars[i] for i in scrips]) <= amount, "Investment in portfolio"
+prob += price['SINO'] * a_1 + price['AAPL'] * a_2 + price['FSAM'] * a_3 <= 3200, \
+        "Investment in portfolio"
 
 
 # Constraints on percentage of portfolio that can be invested in scrip i
-for i in scrips:
-    prob += price[i] * volume_vars[i] <= max_percentage_of_portfolio / 100 * amount
+prob += price['SINO'] * a_1 <= max_percentage_of_portfolio / 100 * amount
+prob += price['AAPL'] * a_2 <= max_percentage_of_portfolio / 100 * amount
+prob += price['FSAM'] * a_3 <= max_percentage_of_portfolio / 100 * amount
+
+
+
 
 
 prob.writeLP("GainPrediction.lp")
@@ -60,7 +63,9 @@ for v in prob.variables():
 
 # The optimised objective function value is printed to the screen
 print "Expected gain % = ", value(prob.objective)
-print "Value of investment", sum([price[k] * v.value() for k, v in volume_vars.items()])
+
+print "Value of investment = ", price['SINO'] * a_1.value() + price['AAPL'] * a_2.value() + \
+                                price['FSAM'] * a_3.value()
 print "------"
 
 # print prob
